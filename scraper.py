4373be829a8a304a2f636187f66d0661f9063796
edenWerke <edenw6292@gmail.com>
2025-07-28 14:26:48 +0300

    prettifing the soup

--- scraper.py
+++ scraper.py
@@ -4,9 +4,9 @@ import requests
 
 
 try:
-    req=requests.get("https://zemenbazaar.com")
+    req=requests.get("https://zemenbazaar.com/")
     req.raise_for_status()  # Ensure we got a successful response
     soup=BeautifulSoup(req.text, 'html.parser')
-    print(soup)
+    print(soup.prettify)
 except Exception as e:
     print(f"An error occurred: {e}")
